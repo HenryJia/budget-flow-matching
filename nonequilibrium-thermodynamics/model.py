41becab9c377aa82e60088b332bf237627ba38e8
Henry Jia <henryjia@protonmail.com>
2026-02-13 06:12:05 +0000

    WIP Start setting up the diffusion model

--- nonequilibrium-thermodynamics/model.py
+++ nonequilibrium-thermodynamics/model.py
@@ -7,9 +7,19 @@ import lightning as L
 
 class DiffusionModel(L.LightningModule):
     def __init__(
-            self, input_dim, input_channels,
-            trajectory_length,
+            self, input_dim, input_channels, layers, hidden_channels,
+            trajectory_length, temporal_basis_size, step1_beta
             ):
+
+        output_channels = input_channels * temporal_basis_size * 2
+        modules = []
+        modules.extend([nn.Conv2d(input_channels, hidden_channels, kernel_size=3, padding=1), nn.LeakyReLU()])
+        for i in range(layers):
+            modules.extend([nn.Conv2d(hidden_channels, hidden_channels, kernel_size=3, padding=1), nn.LeakyReLU()])
+            modules.extend([nn.Conv2d(hidden_channels, hidden_channels, kernel_size=1, padding=0), nn.LeakyReLU()])
+        modules.append(nn.Conv2d(hidden_channels, output_channels, kernel_size=1, padding=0))
+        self.reverse_diffusion = nn.Sequential(*modules)
+
         super(DiffusionModel, self).__init__()
 
     def forward(self, x):
