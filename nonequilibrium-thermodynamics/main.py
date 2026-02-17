ac532b437c9b6ae331c5f77ddea73dcd73090c84
Henry Jia <henryjia@protonmail.com>
2026-02-17 06:08:54 +0000

    WIP almost working

--- nonequilibrium-thermodynamics/main.py
+++ nonequilibrium-thermodynamics/main.py
@@ -5,22 +5,56 @@ import torch.nn as nn
 import torch.utils.data as data
 import torchvision as tv
 import torch.nn.functional as F
+
 import lightning as L
+from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.loggers import WandbLogger
+
+from model import DiffusionModel
+from callbacks import SampleCallback
 
 import wandb
 
+def main(args):
+    with wandb.init(config=args.config, project="nonequilibrium-thermodynamics") as run:
+        if run.config['dataset'] == "MNIST":
+            dataset = tv.datasets.MNIST(root="./data", download=True, transform=tv.transforms.ToTensor())
+        else:
+            raise ValueError(f"Unknown dataset: {run.config['dataset']}")
+
+        dataloader = data.DataLoader(dataset, batch_size=run.config['batchsize'], shuffle=True, num_workers=8)
+
+        model = DiffusionModel(
+            input_channels=run.config['input_channels'],
+            layers=run.config['layers'],
+            hidden_channels=run.config['hidden_channels'],
+            trajectory_length=run.config['trajectory_length'],
+            step1_beta=run.config['step1_beta'],
+            temporal_basis_size=run.config['temporal_basis_size'],
+            lr=run.config['lr']
+        )
+
+        logger = WandbLogger(project="nonequilibrium-thermodynamics", log_model="all")
+
+        checkpoint_callback = ModelCheckpoint(
+            dirpath="./checkpoints",
+            monitor="train_loss",
+            mode="min"
+            )
+        sample_callback = SampleCallback(num_samples=16)
+        trainer = L.Trainer(
+            max_epochs=run.config['epochs'],
+            logger=logger,
+            accelerator='gpu',
+            devices=run.config['gpus'],
+            callbacks=[checkpoint_callback, sample_callback],
+            )
+        trainer.fit(model, dataloader)
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, required=True, help="Path to the config file")
 
     args = parser.parse_args()
 
-    with wandb.init(config=args.config, project="nonequilibrium-thermodynamics") as run:
-        if run.config['dataset'] == "MNIST":
-            dataset = tv.datasets.MNIST(root="./data", download=True, transform=tv.transforms.ToTensor())
-        else:
-            raise ValueError(f"Unknown dataset: {run.config.dataset}")
-
-        dataloader = data.DataLoader(dataset, batch_size=run.config['batchsize'], shuffle=True)
+    main(args)
--- nonequilibrium-thermodynamics/model.py
+++ nonequilibrium-thermodynamics/model.py
@@ -11,6 +11,7 @@ class DiffusionModel(L.LightningModule):
             trajectory_length, step1_beta, temporal_basis_size,
             lr=1e-3
             ):
+        super(DiffusionModel, self).__init__()
         # Note: This is going to be a bit different to the reference theano implementation
         # The reference implementation does a fair bit of more complicated stuff which I think is a tad esoteric
 
@@ -19,8 +20,7 @@ class DiffusionModel(L.LightningModule):
         self.temporal_basis_size = temporal_basis_size
         self.lr = lr
 
-        # I am very unsure we need to use the temporal basis functions at all. The reference implementation does, but it is not clear to me why
-        output_channels = input_channels * 2 #* temporal_basis_size
+        output_channels = input_channels * 2 * temporal_basis_size
 
         # For now, we'll just use a simple convolutional network for the reverse diffusion process
         # We can tune this later to be more efficient and better suited for the task
@@ -33,9 +33,8 @@ class DiffusionModel(L.LightningModule):
         self.reverse_diffusion_net = nn.Sequential(*modules)
 
         # Note: The beta diffusion rate is learnable
-        self.beta = torch.Parameter(self.generate_beta(self, step1_beta))
+        self.beta = nn.Parameter(self.generate_beta(step1_beta))
 
-        super(DiffusionModel, self).__init__()
 
     def generate_beta(self, step1_beta):
         # Generate the beta schedule for the forward diffusion process
@@ -51,25 +50,43 @@ class DiffusionModel(L.LightningModule):
 
 
     def forward_diffusion(self, x, t):
-        # Step 2: Add noise to the input according to the beta schedule
+        # Add noise to the input according to the beta schedule
         # Note that beta for the first step is fixed, and the rest are learnable
         # This is not the most efficient way to do this, but it is the clearest way to implement the forward diffusion process
         # Note, this also starts to deviate from the reference implementation which uses uniform noise for some reason
         # But, the paper describes the forward diffusion process as adding gaussian noise, so we will do that here
         # This is the diffusion kernel from Table App.1 in the paper
-        x_noisy = x 
-        mu = x_noisy 
-        sigma = 0
-        beta = torch.clip(F.softplus(self.beta), min=self.step1_beta) # We use softplus to ensure that beta is positive
-        for i in range(t):
-            x_noisy = x_noisy * torch.sqrt(1 - beta[i]) + torch.randn_like(x) * torch.sqrt(beta[i])
-            mu = mu * torch.sqrt(1 - beta[i])
-            sigma = torch.sqrt(sigma**2 * (1 - beta[i]) + beta[i])
+        x_noisy = x
 
-        return x_noisy, mu, sigma, t
+        beta = torch.clamp(self.beta, min=self.step1_beta, max=1.0-self.step1_beta)
 
+        # OK so this is a tad different to how the paper describes the forward diffusion kernel in Table App.1
+        # But to my understanding, they are equivalent
+        alpha = 1 - beta
+        alpha_cum = torch.gather(torch.cumprod(alpha, dim=0), dim=0, index=t)
+        beta_t = torch.gather(beta, dim=0, index=t)
+        alpha_t = torch.gather(alpha, dim=0, index=t)
+
+        x_noisy = x_noisy * torch.sqrt(alpha_cum)[:, None, None, None] + torch.randn_like(x) * torch.sqrt(1 - alpha_cum)[:, None, None, None]
+
+        # Now we calculate the posterior mean and variance for the reverse diffusion process at timestep t
+        # This is the q(x^(t-1) | x^(t), x^(0)) distribution in Table App.1 in the paper
+        mu0 = x * torch.sqrt(alpha_cum / alpha_t)[:, None, None, None] # prior mean of the forward diffusion process at timestep t-1
+        mu1 = x_noisy / torch.sqrt(alpha_t)[:, None, None, None] # likelihood mean of the forward diffusion process at timestep t-1 from timestep t
+
+        # Note, I'm not entirely certain how the reference implementation derived this, but we'll roll with it
+        covar1 = 1.0 - alpha_cum / alpha_t # prior covariance of the mean of the forward diffusion process at timestep t-1
+        covar2 = beta_t / alpha_t # likelihood covariance of the mean of the forward diffusion process at timestep t-1 from timestep t
+        lam = 1.0 / covar1 + 1.0 / covar2
+
+        mu_posterior = (mu0 / covar1[:, None, None, None] + mu1 / covar2[:, None, None, None]) / lam[:, None, None, None]
+        sigma_posterior = torch.sqrt(1.0 / lam)
+
+        return x_noisy, mu_posterior, sigma_posterior
 
     def reverse_diffusion(self, x_noisy, t):
+        channels = x_noisy.shape[1]
+
         # Step 1: Run the reverse diffusion process through the network to get the predicted noise
         z = self.reverse_diffusion_net(x_noisy)
 
@@ -79,48 +96,69 @@ class DiffusionModel(L.LightningModule):
         # In addition, they use a somewhat odd formula for the reverse diffusion process for images in the Appendix (eq 64 and 65)
         # I don't entirely understand why to be honest
         # All in all, I'm going to ignore all those complications for now, and just use a simple formula for the reverse diffusion process
-        mu = z[:, :x_noisy.shape[1], :, :]
-        sigma = torch.sqrt(F.softplus(z[:, x_noisy.shape[1]:, :, :]))
+        mu = z[:, :channels * self.temporal_basis_size, :, :].view(-1, self.temporal_basis_size, channels, z.shape[2], z.shape[3])
+        sigma = z[:, channels * self.temporal_basis_size:, :, :].view(-1, self.temporal_basis_size, channels, z.shape[2], z.shape[3])
+        sigma = torch.mean(sigma, dim=(2, 3, 4)) # Only one sigma per sample
+
+        # Use a softmax fucntion centred at t to generate the temporal basis functions
+        # This is not quite the same as the reference implementation, but it is simple, dumb, and hopefully will work
+        diff = torch.arange(0, self.temporal_basis_size, step=1.0, device=z.device) - t.view(x_noisy.shape[0], 1).float()
+
+        # This will give us a temporal basis function that peaks near t
+        temporal_basis = 1 - F.softmax(diff**2, dim=1)
+
+        mu = torch.einsum('btchw,bt->bchw', mu, temporal_basis)
+        sigma = torch.einsum('bt,bt->b', sigma, temporal_basis)
+        beta = torch.clamp(self.beta, min=self.step1_beta, max=1.0-self.step1_beta)
+        beta = torch.gather(beta, dim=0, index=t)
+
+        # The paper does this, but I'm not sure why
+        sigma = torch.sqrt(F.sigmoid(sigma + torch.logit(beta)))
+        mu = x_noisy * torch.sqrt(1 - beta[:, None, None, None]) + mu * torch.sqrt(beta[:, None, None, None])
 
         return mu, sigma
 
     def training_step(self, batch, batch_idx):
-        x = batch
+        x = batch[0]
+
         # Step 1: Select a timestep in [1, trajectory_length - 1]
         # Note: We skip t=0 as the reverse process is fixed at the first step
         # Keep the same t for the whole minibatch. I'm a little uncertain about this
         # But, it is how the original reference implemntation in theano does it
-        t = torch.randint(low=1, high=self.trajectory_length, size=(1,), device=x.device)
+        t = torch.randint(low=1, high=self.trajectory_length, size=(x.shape[0],), device=x.device)
 
         # Step 2: Run the forward diffusion process to get the noisy input up to timestep t, the mean and variance of the noise, and the timestep
-        x_noisy, mu_t, sigma_t, t = self.forward_diffusion(x, t)
+        x_noisy, mu_posterior, sigma_posterior = self.forward_diffusion(x, t)
 
         # Step 3: Run the reverse diffusion process to get the predicted mean and variance of the noise
         mu_pred, sigma_pred = self.reverse_diffusion(x_noisy, t)
 
         # Now we are ready to calculate the negative log likelihood bound
         # Step 4: Calculate kl divergence
-        kl = torch.log(sigma_pred) - torch.log(sigma_t) + (sigma_t**2 + (mu_t - mu_pred)**2) / (2 * sigma_pred**2) - 0.5
+        kl = torch.log(sigma_pred)[:, None, None, None] - torch.log(sigma_posterior)[:, None, None, None]
+        kl = kl + (sigma_posterior[:, None, None, None]**2 + (mu_posterior - mu_pred)**2) / (2 * sigma_pred[:, None, None, None]**2) - 0.5
+        kl = kl.mean()
 
         # Step 5: Calculate the entropy of the forward diffusion process at the end of the trajectory
         # This is H_q(X^(T) | X^(0)) in Table App.1 in the paper
         # For this, we'll need to calculate the mean and variance of the forward diffusion process at the end of the trajectory
-        beta = torch.clip(F.softplus(self.beta), min=self.step1_beta)
-        for i in range(t, self.trajectory_length):
-            # mu_t = mu_t * torch.sqrt(1 - beta[i-1]) # No need to calculate the mean for calculating entropy
-            sigma_t = torch.sqrt(sigma_t**2 * (1 - beta[i]) + beta[i])
+        beta = torch.clamp(self.beta, min=self.step1_beta, max=1.0-self.step1_beta)
+        alpha = 1 - beta
+        sigma_1 = torch.sqrt(beta[0])
+        sigma_t = torch.sqrt(1 - torch.exp(torch.sum(torch.log(alpha)))) # More numerically stable
 
+        twopi = torch.tensor(2 * torch.pi, device=x.device) # handy to have this around for calculating entropy
         # We'll abbreviate H_q(X^(T) | X^(0)) as H_qT
-        H_qT = 0.5 * (torch.log(2 * torch.pi) + 2 * torch.log(sigma_t) + 1)
+        H_qT = 0.5 * (torch.log(twopi) + 2 * torch.log(sigma_t) + 1)
 
         # Step 6: Calculate the entropy of the forward diffusion process at t=1
         # This is H_q(X^(1) | X^(0)) in Table App.1 in the paper
         # Note that at t=1, the standard deviation is beta[0]. We don't need to calculate the mean for calculating entropy
-        H_q1 = 0.5 * (torch.log(2 * torch.pi) + 2 * torch.log(beta[0]) + 1)
+        H_q1 = 0.5 * (torch.log(twopi) + 2 * torch.log(sigma_1) + 1)
 
         # Step 7: Add H_p(X^(T)) which is the prior entropy at the end of the forward diffusion process.
         # This is just the entropy of a standard normal distribution
-        H_pT = 0.5 * (torch.log(2 * torch.pi) + 1)
+        H_pT = 0.5 * (torch.log(twopi) + 1)
 
         # Step 8: Put it all together to get the negative log likelihood bound
         # Note that we multiply the KL divergence by the trajectory length as is done in the reference implementation
@@ -129,11 +167,18 @@ class DiffusionModel(L.LightningModule):
         # By the power of sampling different points in the trajectory, by our Lord and Saviour Monte Carlo,
         # we somehow get a good enough estimate of the loss over the whole trajectory.
         log_lower_bound = -kl * self.trajectory_length + H_qT - H_q1 - H_pT
-        loss = -log_lower_bound.mean() # Of course, we want to maximise the log lower bound, so we flip it for SGD
+        loss = -log_lower_bound # Of course, we want to maximise the log lower bound, so we flip it for SGD
 
+        self.log("train_loss", loss, prog_bar=True)
+        self.log("kl", kl.mean(), prog_bar=True)
+        self.log("H_qT", H_qT.mean(), prog_bar=True)
+        self.log("H_q1", H_q1.mean(), prog_bar=True)
+        self.log("sigma_1", sigma_1.mean(), prog_bar=True)
+        self.log("sigma_t", sigma_t.mean(), prog_bar=True)
+        self.log("beta", beta.mean(), prog_bar=True)
         return loss
 
-    def forward(self, x):
+    def forward(self, x, trajectory_length=None):
         # Note: This is technically the reverse diffusion process for sampling the whole trajectory
         # But, PyTorch/Lightning convention means we have to call it forward
 
@@ -141,9 +186,14 @@ class DiffusionModel(L.LightningModule):
         x_noisy = torch.randn_like(x)
 
         # Step 2: Run the reverse diffusion process for the whole trajectory
-        for t in range(self.trajectory_length - 1, -1, -1):
-            mu, sigma = self.reverse_diffusion(x_noisy, t)
-            x_noisy = mu + sigma * torch.randn_like(x)
+        if trajectory_length is None:
+            trajectory_length = self.trajectory_length
+        # Note: without no_grad torch will try to store all the intermediate steps for backprop
+        # Which would blow up the memory. Ignore gradients for sampling.
+        with torch.no_grad():
+            for t in range(trajectory_length - 1, -1, -1):
+                mu, sigma = self.reverse_diffusion(x_noisy, t * torch.ones(x_noisy.shape[0], device=x_noisy.device, dtype=torch.long))
+                x_noisy = mu + sigma[:, None, None, None] * torch.randn_like(x_noisy)
         return x_noisy
 
     def configure_optimizers(self):
